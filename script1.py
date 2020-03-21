64954d480ecb5fef15959aeac35acb26b1b33d4e
tirdeaalex <sanydge@gmail.com>
2020-03-21 21:31:35 +0200

    adding tailwind

--- script1.py
+++ script1.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+class Masina:
+    def __init__(self, motor, roti, usi):
+        self.motor = motor
+        self.roti = roti
+        self.usi = usi * 2
+
+
 echipe = [
     {'nume': 'Proiect 1', "marime": 10, 'bloc': "C10"},
     {'nume': 'Proiect Zero', "marime": 3, 'bloc': "E"},
@@ -12,6 +20,15 @@ echipe = [
 @app.route('/')
 def home():
     return render_template("home.html")
+
+
+@app.route('/test/')
+def test():
+    return render_template("tw.html", garaj=[
+        Masina("2L", '18', 3),
+        Masina("1L", '14', 3.3),
+        Masina("3", '5', 1.3),
+    ])
 
 
 @app.route('/about/')
