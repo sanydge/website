c0a1408a612b25a6239b0979f80ea3e97368f51a
tirdeaalex <sanydge@gmail.com>
2020-03-21 21:07:10 +0200

    Testing stuff

--- script1.py
+++ script1.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+echipe = [
+    {'nume': 'Proiect 1', "marime": 10, 'bloc': "C10"},
+    {'nume': 'Proiect Zero', "marime": 3, 'bloc': "E"},
+    {'nume': 'Hero', "marime": 3, 'bloc': "21"},
+]
+
 
 @app.route('/')
 def home():
@@ -10,7 +16,7 @@ def home():
 
 @app.route('/about/')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", echipe=echipe, display=True)
 
 
 if __name__ == "__main__":
